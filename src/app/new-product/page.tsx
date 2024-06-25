552e7b3ebe85a9c3356c3959cfacaa8b0855dff7
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2024-06-25 17:46:37 +0530

    complete input for new product

--- src/app/new-product/page.tsx
+++ src/app/new-product/page.tsx
@@ -16,12 +16,14 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
   Check,
   ChevronsUpDown,
   ImageUp,
+  Plus,
   SquareChevronDown,
+  Trash2,
 } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -29,18 +31,70 @@ import { new_product } from "@/validators/zod-validators";
 import { zodResolver } from "@hookform/resolvers/zod";
 import toast from "react-hot-toast";
 import { useDropzone } from "react-dropzone";
-import { Account } from "@prisma/client";
+import { Account, Product } from "@prisma/client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const Page = () => {
   // toggle bar
   const [open, setOpen] = useState<boolean>(false);
   const options = ["Shirt", "TShirt", "Pant", "Shoes"] as const;
-  const [value, setValue] = useState<(typeof options)[number] | null>(null);
+  // value for product category
+  const [productCategory, setProductCategory] = useState<
+    (typeof options)[number] | null
+  >(null);
   const [image, setImage] = useState<null | File>(null);
   const [account, setAccount] = useState<Account | null>(null);
+  const [stocks, setStocks] = useState<
+    Array<{ size: string; quantity: number }>
+  >([]);
+  const stockSizeRef = useRef<HTMLInputElement | null>(null);
+  const stockQuantityRef = useRef<HTMLInputElement | null>(null);
+
+  function addNewStockDetails() {
+    if (!stockSizeRef.current || !stockQuantityRef.current) return;
+
+    const size = stockSizeRef.current.value;
+    let quantity = stockQuantityRef.current.value;
+
+    if (!size) {
+      toast.error("Invalid Stock Size");
+      return;
+    }
+    if (!quantity) {
+      toast.error("No stock quantity provided");
+      return;
+    }
+    if (stocks.find((elem) => elem.size === size)) {
+      toast.error("This size stock is already present, remove it first");
+      return;
+    }
+    try {
+      z.coerce.number().nonnegative().int().parse(quantity);
+    } catch (e) {
+      toast.error("Invalid stock quantity!");
+      return;
+    }
+    setStocks([
+      ...stocks,
+      {
+        size,
+        quantity: Number(quantity),
+      },
+    ]);
+  }
+
   const router = useRouter();
 
   const { data: session } = useSession();
@@ -74,7 +128,7 @@ const Page = () => {
     if (!account) {
       return;
     }
-    if (!value) {
+    if (!productCategory) {
       toast.error("Please select the product category");
       setOpen(true);
       return;
@@ -83,15 +137,20 @@ const Page = () => {
       toast.error("No image selected!");
       return;
     }
+    if (stocks.length === 0) {
+      toast.error("Please enter a new stock!");
+      return;
+    }
     const product = {
       name: data.name,
-      category: value,
+      category: productCategory,
       description: data.description,
       // TODO: Make discount part of db, and also allow percentages
       price: data.sale_price - data.discount,
       sellerAccountId: account.id,
       // TODO: Encode image which can be saved in database
       image: image.name,
+      stock: stocks,
     };
     console.log(product);
   };
@@ -223,8 +282,89 @@ const Page = () => {
               </div>
             </div>
 
+            {/* Stock */}
+            <div className="flex items-center justify-between gap-5">
+              <div className="flex-1">
+                <label className="font-semibold" htmlFor="stock-size">
+                  Size
+                </label>
+                <Input id="stock-size" placeholder="XL" ref={stockSizeRef} />
+              </div>
+              <div className="flex-1">
+                <label className="font-semibold" htmlFor="stock-quantity">
+                  Quantity
+                </label>
+                <Input
+                  id="stock-quantity"
+                  placeholder="100"
+                  ref={stockQuantityRef}
+                />
+              </div>
+              <Button
+                variant="secondary"
+                className="mt-5 block"
+                onClick={() => addNewStockDetails()}
+              >
+                <Plus />
+              </Button>
+            </div>
+            {/* Table to show stocks added */}
+            {stocks.length !== 0 && (
+              <Table className="mx-auto my-5 max-w-prose border border-zinc-200">
+                <TableCaption>Stocks Input</TableCaption>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[100px] text-center">
+                      Size
+                    </TableHead>
+                    <TableHead className="text-center">Quantity</TableHead>
+                    {/* Buffer header */}
+                    <TableHead aria-description="remove"></TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {stocks.map((stock) => (
+                    <TableRow key={stock.size}>
+                      <TableCell className="text-center font-medium">
+                        {stock.size}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        {stock.quantity}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        <Button
+                          size="sm"
+                          variant="destructive"
+                          className="px-2 py-1"
+                          onClick={() => {
+                            setStocks(
+                              stocks.filter((ex) => ex.size !== stock.size),
+                            );
+                          }}
+                        >
+                          <Trash2 className="size-4" />
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+                <TableFooter>
+                  <TableRow>
+                    <TableCell className="text-center">Total</TableCell>
+                    <TableCell className="text-center">
+                      {stocks.reduce(
+                        (partial_sum, elem) => partial_sum + elem.quantity,
+                        0,
+                      )}
+                    </TableCell>
+                    <TableCell></TableCell>
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            )}
+
             {/* Separator */}
-            <div aria-hidden className="grow"></div>
+            <div aria-hidden className="min-h-10 grow"></div>
             <div className="mb-10 flex items-center justify-between">
               <div>
                 <Button variant="secondary">Cancel</Button>
@@ -246,13 +386,23 @@ const Page = () => {
           <div>
             <h2 className="font-bold tracking-tight">Product Image</h2>
             {image ? (
-              <div className="relative mb-5 mt-2 aspect-square w-full max-w-52 rounded-lg">
-                <Image
-                  alt=""
-                  src={URL.createObjectURL(image)}
-                  className="rounded-lg object-cover"
-                  fill
-                />
+              <div className="relative">
+                <div className="relative mb-5 mt-2 aspect-square w-full max-w-52 rounded-lg">
+                  <Image
+                    alt=""
+                    src={URL.createObjectURL(image)}
+                    className="rounded-lg object-cover"
+                    fill
+                  />
+                </div>
+                <Button
+                  size="sm"
+                  variant="destructive"
+                  className="absolute bottom-1 right-1"
+                  onClick={() => setImage(null)}
+                >
+                  <Trash2 className="size-5" />
+                </Button>
               </div>
             ) : (
               <div {...getRootProps()} className="mb-5 mt-2">
@@ -287,7 +437,7 @@ const Page = () => {
                   aria-expanded={open}
                   className="w-[200px] justify-between"
                 >
-                  {value ?? "Select category"}
+                  {productCategory ?? "Select category"}
                   <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </Button>
               </PopoverTrigger>
@@ -302,14 +452,16 @@ const Page = () => {
                           value={option}
                           onSelect={(currentValue) => {
                             // @ts-ignore - The value selected will always be valid
-                            setValue(currentValue);
+                            setProductCategory(currentValue);
                             setOpen(false);
                           }}
                         >
                           <Check
                             className={cn(
                               "mr-2 h-4 w-4",
-                              value === option ? "opacity-100" : "opacity-0",
+                              productCategory === option
+                                ? "opacity-100"
+                                : "opacity-0",
                             )}
                           />
                           {option}
