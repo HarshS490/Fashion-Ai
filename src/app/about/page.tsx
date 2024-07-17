28d8d4896abeba47a5be716ccebda98a92322516
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2024-07-17 15:01:57 +0530

    add dev profile except one

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,10 @@
 import WidthWrapper from "@/components/global/MaxWidthWrapper";
 import Navbar from "@/components/global/Navbar";
+
 import { cn } from "@/lib/utils";
 import { Satisfy } from "next/font/google";
 import Image from "next/image";
+import DevCarousel from "./DevCarousel";
 
 const fancy_font = Satisfy({
   weight: "400",
@@ -16,15 +18,15 @@ const Page = () => {
       <main className="mt-5 flex min-h-screen">
         <WidthWrapper>
           {/* About developers */}
-          <div className="my-5 flex items-center justify-between gap-5">
+          <div className="my-5 items-center justify-between gap-5 md:flex">
             <div className="flex-[2]">
-              <h2 className="text-5xl font-semibold">
-                <span className="inline-block">Meet our team of </span>
+              <h2 className="text-center text-4xl font-semibold md:text-left md:text-5xl">
+                <span className="mr-1.5 inline-block">Meet our team of </span>
                 <span className="inline-block">
                   <span
                     className={cn(
                       fancy_font.className,
-                      "text-6xl text-violet-500 drop-shadow-[0_1px_1px_rgba(192,132,252,0.6)]",
+                      "text-5xl text-violet-500 drop-shadow-[0_1px_1px_rgba(192,132,252,0.6)] md:text-6xl",
                     )}
                   >
                     creators
@@ -33,7 +35,7 @@ const Page = () => {
                   <span
                     className={cn(
                       fancy_font.className,
-                      "text-6xl text-fuchsia-600 drop-shadow-[0_1px_1px_rgba(192,132,252,0.6)]",
+                      "text-5xl text-fuchsia-600 drop-shadow-[0_1px_1px_rgba(192,132,252,0.6)] md:text-6xl",
                     )}
                   >
                     designers
@@ -44,24 +46,26 @@ const Page = () => {
                   <span
                     className={cn(
                       fancy_font.className,
-                      "text-6xl text-purple-500 drop-shadow-[0_1px_1px_rgba(192,132,252,0.6)]",
+                      "text-5xl text-purple-500 drop-shadow-[0_1px_1px_rgba(192,132,252,0.6)] md:text-6xl",
                     )}
                   >
                     problem solvers
                   </span>
                 </span>
               </h2>
-              <p className="mt-5 max-w-[80%] text-base text-zinc-700">
+              <p className="mx-auto mt-5 max-w-[80%] text-center text-base text-zinc-700 md:mx-0 md:text-left">
                 We are a team of{" "}
                 <span className="italic">college students</span> who love
                 creating new things. This website is also an attempt to quench
                 this <span className="italic">thirst</span>.
               </p>
             </div>
-            <div className="flex-1" aria-hidden="true">
+            <div className="hidden flex-1 md:block" aria-hidden="true">
               <Image src="/about-hero.svg" alt="" width={400} height={400} />
             </div>
           </div>
+          {/* Carousel of developer profiles */}
+          <DevCarousel />
         </WidthWrapper>
       </main>
     </div>
