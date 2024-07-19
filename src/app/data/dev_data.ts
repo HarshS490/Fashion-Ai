50c3a15661dbcf8b6122e3e86b435400f209e2ef
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2024-07-19 09:52:39 +0530

    add contact page + modularity

---
+++ src/app/data/dev_data.ts
@@ -0,0 +1,34 @@
+export const dev_profiles = [
+  {
+    name: "Harsh",
+    photo: "/next.svg",
+    description: "",
+    role: "Web Developer",
+    linkedin: "https://www.linkedin.com/in/harshsingh1104/",
+    github: "https://github.com/HarshS490",
+  },
+  {
+    name: "Ishan",
+    photo: "/ishan.jpg",
+    description: "",
+    role: "Machine Learning Expert",
+    linkedin: "https://www.linkedin.com/in/ishan-srivastava-baa7a3276/",
+    github: "https://github.com/Ishan130803",
+  },
+  {
+    name: "Lavya",
+    photo: "/lavya.jpg",
+    description: "",
+    role: "Machine Learning Expert",
+    linkedin: "https://www.linkedin.com/in/lavya-thapar-430359261/",
+    github: "https://github.com/Lavya-Thapar",
+  },
+  {
+    name: "Manikya",
+    photo: "/manikya.jpg",
+    description: "",
+    role: "Web Developer",
+    linkedin: "https://www.linkedin.com/in/manikya-sharma-84615125a/",
+    github: "https://github.com/Manikya-Sharma",
+  },
+];
